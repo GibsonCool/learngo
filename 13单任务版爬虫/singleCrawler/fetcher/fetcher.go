ee4fd1d012b76eb0d7569583850fcbfeac0abcdd
DoubleX <double@tg10010.com>
2019-08-05 16:30:57 +0700

    add header 防止用户信息页面请求403

--- 13单任务版爬虫/singleCrawler/fetcher/fetcher.go
+++ 13单任务版爬虫/singleCrawler/fetcher/fetcher.go
@@ -10,8 +10,17 @@ import (
 	根据链接地址读取内容你那个返回
 */
 func Fetch(url string) ([]byte, error) {
+	client := &http.Client{}
 
-	resp, err := http.Get(url)
+	request, e := http.NewRequest(http.MethodGet, url, nil)
+	if e != nil {
+		return nil, e
+	}
+
+	//去该网页上扒取的请求头信息，随时可能失效，防止页面403
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36")
+
+	resp, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
